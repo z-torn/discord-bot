2b5b560f2a0903ec2bb8596edd03fc4f8f0bd17a
StroupBSlayen <strouprd@gmail.com>
2020-01-04 14:03:36 -0800

    Revised database setups

--- bot/database/__init__.py
+++ bot/database/__init__.py
@@ -1,9 +1,7 @@
 "The direcory where database related stuff should go"
 
 from orator import DatabaseManager, Model
-from bot import config
+from bot import bot_instance
 
-# Setup the database manager and model format for Orator
-database = DatabaseManager(config.get("databases"))
-bot.db = database
-Model.set_connection_resolver(database)
+# Setup the Model reference for Orator
+Model.set_connection_resolver(bot_instance.db)
